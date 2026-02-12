0d377e220c9eb5d2d055339fca4e8ec6428a4e28
anilkumbleayaan <152861280+anilkumbleayaan@users.noreply.github.com>
2026-02-12 13:07:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function nextMessage() {
+    document.getElementById("message").innerHTML =
+        "From the day you came into my life... everything changed ❤️<br><br>Will you be my Valentine forever?";
+
+    document.querySelector("button").innerHTML = "YES ❤️";
+}
